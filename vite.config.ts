42f615da4dc9f4cd2385c7798b530a4e1d53d232
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2025-02-02 03:21:04 +0000

    feat: add remix dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { remixDevTools } from "remix-development-tools";
 
 export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
+  plugins: [remixDevTools(),
+    remix(),
     tsconfigPaths(),
   ],
 });
