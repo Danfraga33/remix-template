8cbbc1102401d94797cafc1ed857f40f25e24879
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2025-04-17 21:22:27 +1000

    fix: remove remix dev tools

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { remixDevTools } from "remix-development-tools";
 
 export default defineConfig({
-  plugins: [remixDevTools(),
-    remix(),
+  plugins: [remix(),
     tsconfigPaths(),
   ],
 });
