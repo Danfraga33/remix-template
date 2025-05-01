3ec2d6c640f0759c0a70ac5b80a720a7cc074129
Daniel Fraga <60914504+Danfraga33@users.noreply.github.com>
2025-05-01 10:42:43 +1000

    style: add tailwind typography for custom fonts and md styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,5 +18,5 @@ export default {
       },
     },
   },
-plugins: [require("@tailwindcss/typography")],
+plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
 } satisfies Config;
